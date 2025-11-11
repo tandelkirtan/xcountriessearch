25e821c5a2c4de296e84c1dfdf4df222ebdd8442
tandelkirtan <kirtandel111@gmail.com>
2025-11-11 13:26:25 +0530

    modified h2

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ export const Card = ({common, png}) => {
   return (
     <div className="countryCard">
       <img src={png} alt="country-flag" width="150px" height="110px"/>
-      <h4>{common}</h4>
-    </div>
+      <h2>{common}</h2>
+    </div>  
   )
 }
